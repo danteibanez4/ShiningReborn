dd070387afa18583045037ecdc74f6cfcccb238c
Dante Ibanez <dantedavidibanez2@gmail.com>
2024-07-24 11:53:05 -0700

    Initialized new Sprite Class

---
+++ SpriteTest.java
@@ -0,0 +1,16 @@
+//import select javax packages
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * A small java class that will contain some sprite that will be used in the game world.
+ */
+public class SpriteTest {
+    public int xSprite = 0;
+    public int ySprite = 0;
+
+    public Image sprite;
+
+    //declare constructor and getters/setters for this class (TBA)
+}
